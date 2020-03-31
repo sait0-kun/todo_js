a11e09da0bffffa613f6a687e53d5b14652c22ea
Keita Saito <kitsit.39@gmail.com>
2020-03-31 19:52:07 +0900

    4回目の

--- js/index.js
+++ js/index.js
@@ -5,19 +5,28 @@
     const Table = document.getElementById('targetTable');
     const NewRow = Table.insertRow();
     const Td = document.createElement('td');
+
+    const Date = document.getElementById('Date'); //日付取得
+    const Text = document.getElementById('Text'); //内容取得
+    const CompleteButton = '<input type="button" value="完了" onclick="completeRow(this)" />'; //完了ボタン生成
+    const DeleteButton = '<input type="button" value="削除" onclick="deleteRow(this)" />'; //削除ボタン生成
+
+    
+    // 日付追加
     let newCell = NewRow.insertCell();
-
-    const Date = document.getElementById('Date')
-    const Text = document.getElementById('Text')
-
-    // 日付を追加
     newCell.textContent = Date.value;
-    newCell.appendChild(Td);
 
-    // textを追加
+    // text追加
     newCell = NewRow.insertCell();
     newCell.textContent = Text.value;
-    newCell.appendChild(Td);
+
+    // 完了ボタン追加
+    newCell = NewRow.insertCell();
+    newCell.innerHTML = CompleteButton;
+
+    // 削除ボタン追加
+    newCell = NewRow.insertCell();
+    newCell.innerHTML = DeleteButton;
 
     // textクリア&focus
     Text.value = '';
