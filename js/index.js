296b076419e9d8f21799a745916714f30aa0e3d7
Keita Saito <kitsit.39@gmail.com>
2020-04-03 22:36:11 +0900

    改善の余地あり

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 'use strict'
 let i = 0;
-let strage = localStorage;
+let storage = localStorage;
 let newCell = null;
 const table = document.getElementById('targetTable');
 const complete_button = '<input type="button" value="完了" class="btn" onclick="completeRow(this)" />'; //完了ボタン生成
 const delete_button = '<input type="button" value="削除" class="btn" onclick="deleteRow(this)" />'; //削除ボタン生成
 
 {
-  for (i = 0; i < strage.length; i++ ) {
+  for (i = 0; i < storage.length; ++i ) {
     const new_row = table.insertRow();
     newCell = new_row.insertCell();
-    newCell.textContent = strage.getItem(i);
+    newCell.textContent = storage.getItem(i);
     newCell = new_row.insertCell();
     newCell.innerHTML = complete_button;
     newCell = new_row.insertCell();
@@ -20,14 +20,16 @@ const delete_button = '<input type="button" value="削除" class="btn" onclick="
 
 {
   document.getElementById('btn').addEventListener('click', () => {
-    // const td = document.createElement('td');
+    for (i = 0; i < storage.length; i++) {
+
+    }
     const text = document.getElementById('Text'); //内容取得
     const new_row = table.insertRow();
     
     // text追加
     newCell = new_row.insertCell();
     newCell.textContent = text.value;
-    strage.setItem(i, text.value);
+    storage.setItem(i, text.value);
 
     // 完了ボタン追加
     newCell = new_row.insertCell();
