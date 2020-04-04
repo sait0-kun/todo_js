9821d714b808ea5e5c55576181e54cd3a2de4ad5
Keita Saito <kitsit.39@gmail.com>
2020-04-04 21:52:47 +0900

    デザインOK

--- js/index.js
+++ js/index.js
@@ -1,22 +1,25 @@
 'use strict'
+
 let i = 0;
 let storage = localStorage;
 let newCell = null;
 const table = document.getElementById('targetTable');
-const complete_button = '<input type="button" value="完了" class="btn" onclick="completeRow(this)" />'; //完了ボタン生成
-const delete_button = '<input type="button" value="削除" class="btn" onclick="deleteRow(this)" />'; //削除ボタン生成
+const complete_button = '<i class="fas fa-check" onclick="completeRow(this)"></i>'; //完了ボタン生成
+const delete_button = '<i class="fas fa-trash-alt" onclick="deleteRow(this)"></i>'; //削除ボタン生成
 
 {
   for (i = 0; i < storage.length; ++i ) {
     const new_row = table.insertRow();
     newCell = new_row.insertCell();
-    newCell.textContent = storage.getItem(i);
-    newCell = new_row.insertCell();
     newCell.innerHTML = complete_button;
+    newCell = new_row.insertCell();
+    newCell.textContent = storage.getItem(i);
     newCell = new_row.insertCell();
     newCell.innerHTML = delete_button;
   }
 }
+
+
 
 {
   document.getElementById('btn').addEventListener('click', () => {
@@ -26,14 +29,14 @@ const delete_button = '<input type="button" value="削除" class="btn" onclick="
     const text = document.getElementById('Text'); //内容取得
     const new_row = table.insertRow();
     
+    // 完了ボタン追加
+    newCell = new_row.insertCell();
+    newCell.innerHTML = complete_button;
+
     // text追加
     newCell = new_row.insertCell();
     newCell.textContent = text.value;
     storage.setItem(i, text.value);
-
-    // 完了ボタン追加
-    newCell = new_row.insertCell();
-    newCell.innerHTML = complete_button;
 
     // 削除ボタン追加
     newCell = new_row.insertCell();
