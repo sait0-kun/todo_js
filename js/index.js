087136b4c580dd2328704fced96827964f55db69
Keita Saito <kitsit.39@gmail.com>
2020-04-06 21:23:50 +0900

    リロード時処理、修正

--- js/index.js
+++ js/index.js
@@ -8,23 +8,26 @@ const complete_button = '<i class="fas fa-check" onclick="completeRow(this)"></i
 const delete_button = '<i class="fas fa-trash-alt" onclick="deleteRow(this)"></i>'; //削除ボタン生成
 
 {
-  for (i = 0; i < storage.length; ++i ) {
+  //リロード時のhtml組み立て処理
+  for (i = 0; i < storage.length; i++ ) { //localStorage内のitem数、繰り返す
+    let insertItem = { //itemをset
+      com_btn: complete_button,
+      sto: storage.getItem(i),
+      del_btn: delete_button
+    };
     const new_row = table.insertRow();
-    newCell = new_row.insertCell();
-    newCell.innerHTML = complete_button;
-    newCell = new_row.insertCell();
-    newCell.textContent = storage.getItem(i);
-    newCell = new_row.insertCell();
-    newCell.innerHTML = delete_button;
+    
+    for (let key in insertItem) { //insertItem内の配列を組み立て
+      newCell = new_row.insertCell();
+      newCell.innerHTML = insertItem[key];
+    }
   }
 }
-
-
 
 {
   document.getElementById('btn').addEventListener('click', () => {
     for (i = 0; i < storage.length; i++) {
-
+      //storageのkeyを最後の番号に設定
     }
     const text = document.getElementById('Text'); //内容取得
     const new_row = table.insertRow();
